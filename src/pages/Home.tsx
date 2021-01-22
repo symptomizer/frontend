633eb2e558655f68553044dbb1469f089eb7d6e0
Greg Brimble <developer@gregbrimble.com>
2021-01-22 14:23:12 +0000

    Fix color contrast accessibility on footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -510,7 +510,7 @@ export const Home: FC = () => {
         </main>
         <footer>
           <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-            <div className="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
+            <div className="border-t border-gray-200 py-8 text-sm text-gray-600 text-center sm:text-left">
               <span className="block sm:inline">© 2021 Symptomizer.</span>{" "}
               <span className="block sm:inline">All rights reserved.</span>
             </div>
