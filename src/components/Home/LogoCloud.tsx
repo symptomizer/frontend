dc56028fabb445f5f1e09421b8019d853c541ebb
Mohammad Javad <jkhazali@gmail.com>
2021-02-04 08:06:51 +0000

    Hero and Logo Cloud Edits.

--- src/components/Home/LogoCloud.tsx
+++ src/components/Home/LogoCloud.tsx
@@ -10,26 +10,34 @@ export const LogoCloud: FC = () => (
       Featuring content exclusively from reputable sources
     </p>
     <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
-      <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-nhs-blue">
-        <svg className="max-h-12">
-          <NHS />
-        </svg>
-      </div>
-      <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-nice-black">
-        <svg className="max-h-12">
-          <NICE />
-        </svg>
-      </div>
-      <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-who-blue">
-        <svg className="max-h-12">
-          <WHO />
-        </svg>
-      </div>
-      <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-bnf-black">
-        <svg className="max-h-12">
-          <BNF />
-        </svg>
-      </div>
+      <a href="https://www.nhs.uk/">
+        <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-nhs-blue">
+          <svg className="max-h-12">
+            <NHS />
+          </svg>
+        </div>
+      </a>
+      <a href="https://www.nice.org.uk/">
+        <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-nice-black">
+          <svg className="max-h-12">
+            <NICE />
+          </svg>
+        </div>
+      </a>
+      <a href="https://www.who.int/">
+        <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-who-blue">
+          <svg className="max-h-12">
+            <WHO />
+          </svg>
+        </div>
+      </a>
+      <a href="https://bnf.nice.org.uk/">
+        <div className="transition duration-150 ease-in-out col-span-1 flex justify-center py-8 px-8 bg-gray-50 text-gray-400 hover:text-brands-bnf-black">
+          <svg className="max-h-12">
+            <BNF />
+          </svg>
+        </div>
+      </a>
       <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50"></div>
       <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50"></div>
     </div>
